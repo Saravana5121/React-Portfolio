68121e0586c09763b38285a813daf38f9d756d6a
Sana <darkking7806@gmail.com>
2024-04-30 09:36:18 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <>
-    <div className="text-red-500 text-3xl">Hello world</div>
-    </>
-  )
-}
+    <div className="overflow-x-hidden text-neutral-900 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full">
+        <div className="absolute top-0 -z-10 h-full w-full bg-white">
+          <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
+        </div>
+      </div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
